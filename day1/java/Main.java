0bc0d7bddcacf44811e846b5649ec4a632c4696e
Nicolas Osorio <osorionicolas95@gmail.com>
2024-01-28 19:32:13 +0100

    add day 3

--- day1/java/Main.java
+++ day1/java/Main.java
@@ -45,6 +45,7 @@ public class Main {
                     }
                     return line;
                 }));
+
     }
 
     private static String apply(String line) {
--- day3/java/Main.java
+++ day3/java/Main.java
@@ -3,15 +3,21 @@ package day3.java;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
+    List<String> symbols = List.of("*", "#", "$", "+");
     public static void main(String[] args) throws IOException {
         //String file = "day3/input";
         String file = "day3/example1";
         //String file = "day3/example2";
-        Stream<String> stream1 = Files.lines(Paths.get(file));
+        List<String> stream1 = Files.readAllLines(Paths.get(file));
         long start1 = System.currentTimeMillis();
         Integer part1 = part1(stream1);
         long end1 = System.currentTimeMillis();
@@ -26,7 +32,26 @@ public class Main {
         System.out.println(part2);*/
     }
 
-    private static Integer part1(Stream<String> lines) {
+    private static Integer part1(List<String> lines) {
+        List<IntStream> nRanges = new ArrayList<>();
+        List<IntStream> sRanges = new ArrayList<>();
+
+        for(String line : lines) {
+            Pattern pDots = Pattern.compile("\\.+");
+            Pattern pNumber = Pattern.compile("\\d{1,3}");
+            Pattern pSymbols = Pattern.compile("[*#+$]");
+            System.out.println("-------NUMBERS------");
+
+            Matcher mn = pNumber.matcher(line);
+            while(mn.find()) {
+                nRanges.add(IntStream.rangeClosed(mn.start(), mn.end()));
+            }
+            System.out.println("-------SYMBOLS------");
+            Matcher ms = pSymbols.matcher(line);
+            while(ms.find()) {
+                sRanges.add(IntStream.rangeClosed(ms.start(), ms.end()));
+            }
+        }
         return 0;
     }
 
@@ -34,4 +59,7 @@ public class Main {
         return 0;
     }
 
+    private static boolean isOverlapping(Integer x1, Integer x2, Integer y1, Integer y2) {
+        return Math.max(x1,y1) <= Math.min(x2,y2);
+    }
 }
